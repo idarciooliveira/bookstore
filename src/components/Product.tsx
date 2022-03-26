a06cbc3a7305fb96e2a9c7ce7be879a2e4a115fd
idarciooliveira <idarciooliveira@gmail.com>
2022-03-26 23:49:01 +0100

    added hover on product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,7 +13,7 @@ const Product: NextPage<ProductProp> = ({ product: { title, image_url, slug, cat
     return (
         <div onClick={() => {
             router.push(`/books/${slug}`)
-        }} className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
+        }} className="mt-6 hover:brightness-50 cursor-pointer lg:mt-0 lg:px-2 lg:w-4/5 ">
             <div className="flex flex-col items-center justify-center w-full max-w-lg mx-auto">
                 <img className="object-cover w-full rounded-md h-72 xl:h-80" src={image_url} alt="T-Shirt" />
                 <h4 className="mt-2 text-base font-medium text-gray-700 dark:text-gray-200">{title}</h4>
